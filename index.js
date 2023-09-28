d14a99f6e6c5e62ab60562107e4407bb5f9357aa
Konok majamder <58282427+konok101@users.noreply.github.com>
2023-09-28 07:39:54 +0600

    proces added

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const app = express()
+require('dotenv').config()
 const mongoose = require('mongoose');
 require('./src/dbCon')
 const productRouter = require('./src/routes/Producrs');
 const userRouter = require('./src/routes/Users')
-const port = 5000;
+const Port = process.env.Port || 500;
 
 app.use(express.json())
 app.use(productRouter)
@@ -15,6 +16,6 @@ app.get('/', (req, res) => {
     res.send('Hello I am learning to mongodb mongoose!')
 })
 
-app.listen(port, () => {
+app.listen(Port, () => {
     console.log(`mongoose app listening on port ${port}`)
 })
